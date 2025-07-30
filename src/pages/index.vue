461cf80ba94a2fbb86ad975116e1a5bd35af3bb7
shahab00x <shahab.feb.07.1988@gmail.com>
2025-07-30 22:14:58 +0330

    ui fixes

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -707,6 +707,7 @@ function handleTouchEnd(event: TouchEvent, index: number) {
               v-if="!isMobile"
               class="clipboard-remove-btn"
               title="Remove verse"
+              style="background-color: #dc2626; color: white;"
               @click="removeFromClipboard(index)"
             >
               <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
